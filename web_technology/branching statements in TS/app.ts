e492f3f517e99f8bb66913e54ca9d6fe0abe41b4
Suseela <suseela230304@gmail.com>
2026-04-21 20:20:54 +0530

    app.ts

--- web_technology/branching statements in TS/app.ts
+++ web_technology/branching statements in TS/app.ts
@@ -1,44 +1,70 @@
-const executeTask = () =&gt; {
-const numInput = document.getElementById(&quot;numInput&quot;) as HTMLInputElement;
-const caseInput = document.getElementById(&quot;caseInput&quot;) as HTMLInputElement;
-const resultArea = document.getElementById(&quot;result&quot;) as HTMLElement;
-const n = parseInt(numInput.value);
-const choice = parseInt(caseInput.value);
-let output = &quot;&quot;;
-switch (choice) {
-case 1: // Even series using for loop
-output = &quot;Even Series: &quot;;
-for (let i = 0; i &lt;= n; i++) {
-if (i % 2 === 0) output += i + &quot; &quot;;
-}
+function process(): void {
 
-break;
-case 2: // Fibonacci series using while loop
-output = &quot;Fibonacci Series: &quot;;
-let a = 0, b = 1, next = 0;
-while (a &lt;= n) {
-output += a + &quot; &quot;;
-next = a + b;
-a = b;
-b = next;
-}
-break;
-case 3: // Strong Number check using do...while loop
-let temp = n;
-let sum = 0;
-do {
-let digit = temp % 10;
-let fact = 1;
-for (let i = 1; i &lt;= digit; i++) fact *= i;
-sum += fact;
-temp = Math.floor(temp / 10);
-} while (temp &gt; 0);
+    let num = Number((document.getElementById("num") as HTMLInputElement).value);
+    let choice = Number((document.getElementById("choice") as HTMLSelectElement).value);
 
-output = (sum === n) ? `${n} is a Strong Number.` : `${n} is NOT a Strong Number.`;
-break;
-default:
-output = `Checked number: ${n}. Please select a valid case (1, 2, or 3).`;
+    let result = "";
+
+    switch (choice) {
+
+        case 1:
+            // Even Series using for loop
+            result = "Even Series: ";
+
+            for (let i = 2; i <= num; i += 2) {
+                result += i + " ";
+            }
+
+            break;
+
+        case 2:
+            // Fibonacci Series using while loop
+            let a = 0, b = 1, i = 0;
+
+            result = "Fibonacci Series: ";
+
+            while (i < num) {
+                result += a + " ";
+
+                let temp = a + b;
+                a = b;
+                b = temp;
+
+                i++;
+            }
+
+            break;
+
+        case 3:
+            // Strong Number using do...while loop
+
+            let temp = num;
+            let sum = 0;
+
+            do {
+
+                let digit = temp % 10;
+                let fact = 1;
+
+                for (let j = 1; j <= digit; j++) {
+                    fact = fact * j;
+                }
+
+                sum = sum + fact;
+                temp = Math.floor(temp / 10);
+
+            } while (temp > 0);
+
+            if (sum == num)
+                result = num + " is a Strong Number";
+            else
+                result = num + " is NOT a Strong Number";
+
+            break;
+
+        default:
+            result = "Please enter a valid choice";
+    }
+
+    document.getElementById("result")!.innerHTML = result;
 }
-resultArea.innerText = output;
-};
-document.getElementById(&quot;btn&quot;)?.addEventListener(&quot;click&quot;, executeTask);
