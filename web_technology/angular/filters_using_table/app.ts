1bdf3ebda1c741adf5b4e5199d9e0b8926750a20
Suseela <suseela230304@gmail.com>
2026-04-23 15:05:23 +0530

    app.ts

---
+++ web_technology/angular/filters_using_table/app.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  name = "Angular Framework";
+  amount = 25000;
+  today = new Date();
+}
